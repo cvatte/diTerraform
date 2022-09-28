92ad8ec2a181d7f6b40a76998015886e2a286a39
chaitanyavatte <chaitanyavatte77@gmail.com>
2022-09-28 16:50:30 +0530

    script modified

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt install default-jre -y
+wait
 sudo mkdir -p /data/dictrl
 sudo useradd -d /data/dictrl -m dictrl -p clearsense@123
 sudo su - root
@@ -6,6 +8,3 @@ cp /etc/skel/.* /data/dictrl/
 sleep 5
 chown dictrl:dictrl -R /data/dictrl/
 # sleep 10
-sudo apt install default-jre -y
-wait
-sleep 3
