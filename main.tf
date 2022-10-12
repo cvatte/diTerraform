ef909be3676dd401d958b6b8359477a253beab69
cvatte <108267923+cvatte@users.noreply.github.com>
2022-10-12 14:03:47 +0530

    Update main.tf
    removed subscription tenant client and client secret due security issue

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 #specifies provider configurations for the providers being used in this project
 provider "azurerm" {
  features {}
- subscription_id = "d407f769-7b0c-4807-b509-5e96f3978c76"
- tenant_id       = "c4a3076e-e94c-4b3e-a9d9-de6baf0d3604"
- client_id       = "d4f2dfdd-e60a-4bef-a60b-5cda8918c359"
- client_secret   = "M9O8Q~TnYMcri5T2qFw4.fCuBI.mmNs8yMZGvaa8"
+ subscription_id = ""
+ tenant_id       = ""
+ client_id       = ""
+ client_secret   = ""
 }
 
 #specifies provider configurations for the providers being used in this project
